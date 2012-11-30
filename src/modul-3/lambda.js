16b08cf52e25dba9991fc552e8925ab047e19176
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2012-11-30 20:04:49 +0700

    Bab 3 - Lambda Expression + contoh

---
+++ src/modul-3/lambda.js
@@ -0,0 +1,25 @@
+// Diambil dari 
+// http://stackoverflow.com/questions/3865335/what-is-a-lambda-language
+// dengan beberapa perubahan
+
+function applyOperation(a, b, operation) {
+	return operation(a, b);
+}
+
+function add(a, b) {
+	return a+b;
+}
+
+function subtract(a, b) {
+	return a-b;
+}
+
+console.log('1,2, add: ' + applyOperation(1,2, add));
+console.log('43,21, subtract: ' + applyOperation(43,21, subtract));
+
+console.log('4^3: ' + applyOperation(4, 3, function(a,b) {return Math.pow(a, b)}))
+
+// hasil:
+// 1,2, add: 3
+// 43,21, subtract: 22
+// 4^3: 64
