2fffe6372c28aab7d04101070e4561609e7d0140
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2012-12-09 16:51:09 +0700

    Bab 5 -> (A)Synchronous programming

--- src/modul-5/synchronous.js
+++ src/modul-5/synchronous.js
@@ -2,8 +2,6 @@ var fs = require('fs');
 var sys = require('sys');
 
 sys.puts('Mulai baca file');
-fs.readFile('./pegawai.json', "utf-8",  function(err, data) {
-  if (err) throw err;
-  console.log(data);
-})
+data = fs.readFileSync('./pegawai.json', "utf-8");
+console.log(data);
 sys.puts('Baris setelah membaca file');
